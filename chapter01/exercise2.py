482b653e76f36e58947f32d010a80ed7674182a1
Jerry Khalil <khaliljt09@gmail.com>
2025-11-30 17:32:07 -0600

    Add chapter01 exercises and created chapter02

---
+++ chapter01/exercise2.py
@@ -0,0 +1,9 @@
+# create a tuple called names that contains several pet names
+pet_names = (
+    "Rusty",
+    "Bella",
+    "Buddy",
+    "Whiskers",
+)
+print(pet_names)
+print(type(pet_names))
---
+++ chapter01/exercise3.py
@@ -0,0 +1,13 @@
+# Create a dictionary named pets that contains
+# a list of pet names and the type of animal
+
+pet_names = {
+    "Buddy": "dog",
+    "Whiskers": "cat",
+    "Bella": "dog",
+    "Pepper": "rabbit",
+    "Chocobo": "bird",
+}
+
+print(pet_names)
+print(type(pet_names))
